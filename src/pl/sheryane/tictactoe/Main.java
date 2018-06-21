e6bd43be3e5fe0ba0b35c87044927e4caa4e651e
sheryane <aetherna@interia.pl>
2018-06-21 19:02:16 +0200

    Working pane with 9 tiles + X/O draw

--- src/pl/sheryane/tictactoe/Main.java
+++ src/pl/sheryane/tictactoe/Main.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 
 public class Main extends Application {
 
@@ -31,7 +34,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Tic Tac Toe");
         primaryStage.setScene(new Scene(createContent()));
@@ -39,13 +42,38 @@ public class Main extends Application {
     }
 
     private class Tile extends StackPane {
-        public Tile () {
-            Rectangle border = new Rectangle(200, 200);
+
+        private Text text = new Text();
+
+        public Tile() {
+            Rectangle border = new Rectangle(100, 100);
             border.setFill(null);
             border.setStroke(Color.BLACK);
 
+            text.setFont(Font.font(48));
+
             setAlignment(Pos.CENTER);
-            getChildren().addAll(border);
+            getChildren().addAll(border, text);
+
+            // This code will be executed, when the mouse is clicked on the tile.
+
+            setOnMouseClicked(event -> {
+                //PRIMARY is the left and SECONDARY is the right mouse button.
+                if (event.getButton() == MouseButton.PRIMARY) {
+                    drawX();
+                }
+                else if (event.getButton() == MouseButton.SECONDARY) {
+                    drawO();
+                }
+            });
+        }
+
+        private void drawX() {
+            text.setText("X");
+        }
+
+        private void drawO() {
+            text.setText("O");
         }
     }
 
